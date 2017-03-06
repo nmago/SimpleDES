fe944abafbd148a924ba74db8c041a87454d1b5e
nmago <e-five@mail.ru>
2017-03-06 19:41:18 +0300

    Test ok

--- src/ru/nmago/crypt/sdes/Main.java
+++ src/ru/nmago/crypt/sdes/Main.java
@@ -5,7 +5,8 @@ import ru.nmago.crypt.sdes.SDESKeyGen;
 public class Main {
 
     public static void main(String[] args) {
-	    SDESKeyGen kg = new SDESKeyGen(717); //test on key=717
+	    SDESKeyGen kg = new SDESKeyGen(717); //test on key = 717
 	    kg.generate();
+	    
     }
 }
